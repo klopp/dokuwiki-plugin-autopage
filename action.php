12e96e61832caf5af720b6a5d8b3285dcf7bce80
klopp <klopp@yandex.ru>
2015-05-29 03:15:39 +0300

    class name

--- action.php
+++ action.php
@@ -9,7 +9,7 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
-class action_plugin_autostartpage extends DokuWiki_Action_Plugin {
+class action_plugin_autons extends DokuWiki_Action_Plugin {
 
     /**
      * Registers a callback function for a given event
@@ -19,7 +19,7 @@ class action_plugin_autostartpage extends DokuWiki_Action_Plugin {
      */
     public function register(Doku_Event_Handler &$controller) 
     {
-       $controller->register_hook('IO_NAMESPACE_CREATED', 'AFTER', $this, 'autostartpage_handle');
+       $controller->register_hook('IO_NAMESPACE_CREATED', 'AFTER', $this, 'autons_handle');
     }
 
     /**
@@ -32,7 +32,7 @@ class action_plugin_autostartpage extends DokuWiki_Action_Plugin {
      * @return void
      */
 
-    public function autostartpage_handle(Doku_Event &$event, $param) 
+    public function autons_handle(Doku_Event &$event, $param) 
     {
         global $conf;
         global $INFO;
