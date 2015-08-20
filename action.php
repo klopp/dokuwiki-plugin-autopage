a881f91eaeec8c82cddc9374b6444e9113e2638f
klopp <klopp@yandex.ru>
2015-08-20 13:55:54 +0300

    cosmetic

--- action.php
+++ action.php
@@ -3,7 +3,7 @@
  * DokuWiki Plugin autopage (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
- * @author  Vsevolod Lutovinov <klopp@yandex.ru>
+ * @author  Vseроvolod Lutovinov <klopp@yandex.ru>
  * 
  * This plugin is evolution of autostartpage plugin:
  *  http://dokuwiki.org/plugin:autostartpage 
